3d92c8b591eb08ea04b8f045d9e1305dc5b55402
Ray Ockenfels <ockenfelsr@gmail.com>
2020-06-28 01:00:30 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,14 +1,11 @@
 class EmailAddressParser(input_file)
   
-  attr_accessor :emails, :input_file
-  
   def initialize
     @input_file = input_file
-    self.parse(@input_file)
+    @emails = self.parse(@input_file)
   end
   
   def parse(data)
-    @emails = data.split(/[, ]/)
-    @emails
+    data.split(/[, ]/)
   end
 end
